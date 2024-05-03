ab422e1e63fb99b69b74b4a1f01c331e1959bf80
leh <kkh60245@gmail.com>
2024-05-03 02:19:44 +0000

    잘못된 코드 수정

--- datasets.py
+++ datasets.py
@@ -12,7 +12,7 @@ def build_dataset(is_train, args):
 
     transform = build_transform(is_train, args)
     root = os.path.join(args.data_path, "train" if is_train else "val")
-    if args.is_test is not None:
+    if args.is_test:
         transform = build_transform(False, args)
         root = os.path.join(args.data_path, "test")
     dataset = datasets.ImageFolder(root, transform=transform)
